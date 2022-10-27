630d1335840dd3742ba0303c3af1eb2e78572167
Soma Zsják <sanraith@users.noreply.github.com>
2022-10-27 20:50:08 +0200

    Replace mod.rs files with <module_name>.rs.

--- aoc_lib/build.rs
+++ aoc_lib/build.rs
@@ -6,15 +6,14 @@ type GenericResult<T> = Result<T, Box<dyn Error>>;
 const MODULE_DEFINITIONS_PLACEHOLDER: &'static str = "__MODULE_DEFINITIONS__";
 const SOLUTION_TYPE_LIST_PLACEHOLDER: &'static str = "__SOLUTION_TYPE_LIST__";
 const SOLUTION_TYPE_LIST_APPEND_PLACEHOLDER: &'static str = "__SOLUTION_TYPE_LIST_APPEND__";
+const MODULE_NAME_PLACEHOLDER: &'static str = "__MODULE_NAME__";
 const RE_EXPORTS_PLACEHOLDER: &'static str = "__RE_EXPORTS__";
 
 const SOLUTION_DIRECTORY: &'static str = "src/solutions/";
-const SOLUTION_MODULE_NAME: &'static str = "mod.rs";
 const SOLUTION_MODULE_TEMPLATE_PATH: &'static str = "templates/solution/mod.rs.template";
-
 const TEST_DIRECTORY: &'static str = "src/tests/solutions/";
-const TEST_MODULE_NAME: &'static str = "mod.rs";
-const TEST_MODULE_TEMPLATE_PATH: &'static str = "templates/test/solutions.rs.template";
+const TEST_MODULE_TEMPLATE_PATH: &'static str = "templates/test/mod.rs.template";
+const RELATIVE_MODULE_FILE_NAME: &'static str = "../__MODULE_NAME__.rs";
 
 fn main() {
     println!("cargo:rerun-if-changed={}", SOLUTION_DIRECTORY);
@@ -52,8 +51,7 @@ fn generate_source_module(solution_dir: &str) -> GenericResult<()> {
         .filter_map(|x| x.ok())
         .filter(|x| x.path().is_dir());
     for entry in directories {
-        let path = entry.path();
-        let path_str = path.to_str().unwrap().to_owned();
+        let path_str = entry.path().to_str().unwrap().to_owned();
         generate_source_module(&path_str)?;
         let mod_name = entry.file_name().to_str().unwrap().to_owned();
         module_lines.push(format!("pub mod {};", mod_name));
@@ -82,7 +80,7 @@ fn generate_source_module(solution_dir: &str) -> GenericResult<()> {
         &vec_append_lines.join("\n"),
     );
 
-    let path = PathBuf::from_iter([solution_dir, SOLUTION_MODULE_NAME]);
+    let path = get_module_file_name(solution_dir);
     let mut file = std::fs::File::create(path)?;
     file.write_all(output.as_bytes())?;
 
@@ -100,8 +98,7 @@ fn generate_test_module(test_dir: &str) -> GenericResult<()> {
         .filter_map(|x| x.ok())
         .filter(|x| x.path().is_dir());
     for entry in directories {
-        let path = entry.path();
-        let path_str = path.to_str().unwrap().to_owned();
+        let path_str = entry.path().to_str().unwrap().to_owned();
         generate_test_module(&path_str)?;
         let mod_name = entry.file_name().to_str().unwrap().to_owned();
         module_lines.push(format!("pub mod {};", mod_name));
@@ -114,11 +111,24 @@ fn generate_test_module(test_dir: &str) -> GenericResult<()> {
         &module_lines.join("\n"),
     );
 
-    let path = PathBuf::from_iter([test_dir, TEST_MODULE_NAME]);
+    let path = get_module_file_name(test_dir);
     let mut file = std::fs::File::create(path)?;
     file.write_all(output.as_bytes())?;
 
     Ok(())
+}
+
+fn get_module_file_name(module_dir: &str) -> PathBuf {
+    let module_name = PathBuf::from(module_dir)
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_owned();
+    let mut file_name = RELATIVE_MODULE_FILE_NAME.to_owned();
+    replace_placeholder(&mut file_name, MODULE_NAME_PLACEHOLDER, &module_name);
+    let path = PathBuf::from_iter([module_dir, &file_name]);
+    path
 }
 
 fn collect_modules_from_dir(
