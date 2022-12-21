3c9278e642c5d3638cad1e6d01fe0d93c72b886e
Soma Zsják <sanraith@users.noreply.github.com>
2022-12-21 12:53:30 +0100

    WIP Day 21 part 2.

--- aoc-lib/src/solutions/year2022/day21.rs
+++ aoc-lib/src/solutions/year2022/day21.rs
@@ -12,17 +12,224 @@ impl Solution for Day21 {
 
     fn part1(&mut self, ctx: &Context) -> SolutionResult {
         let monkeys = parse_input(ctx)?;
-        let root = calc_monkey(&"root".to_owned(), &monkeys, &mut HashMap::new())?;
+        let root = calc_monkey("root", &monkeys, &mut HashMap::new())?;
         Ok(root.to_string())
     }
 
-    fn part2(&mut self, _ctx: &Context) -> SolutionResult {
-        Err(NotImplementedError)?
+    fn part2(&mut self, ctx: &Context) -> SolutionResult {
+        let mut monkeys = parse_input(ctx)?;
+        let root = monkeys.get("root").ok_or("no root")?;
+        let (expr_left, expr_right) = match root {
+            Monkey::Add(a, b) | Monkey::Sub(a, b) | Monkey::Mul(a, b) | Monkey::Div(a, b) => (a, b),
+            _ => Err("invalid root")?,
+        };
+        monkeys.insert(
+            "root".to_owned(),
+            Monkey::Equ(expr_left.to_owned(), expr_right.to_owned()),
+        );
+        let s = print_eq_simplified("root", &monkeys, &mut HashMap::new());
+        println!("{:?}", s);
+
+        // let mut c = Computer::<f64>::default();
+        // println!("{}", c.eval(&s).unwrap());
+
+        let root = monkeys.get("root").ok_or("no root")?;
+        let (expr_left, expr_right) = match root {
+            Monkey::Equ(a, b) => (a, b),
+            _ => Err("invalid root")?,
+        };
+        let left = calc_monkey_2(expr_left, &monkeys, &mut HashMap::new());
+        let right = calc_monkey_2(expr_right, &monkeys, &mut HashMap::new());
+        let (mut left, mut right) = match (left, right) {
+            (Err(_), Ok(constant)) => (expr_left, constant),
+            (Ok(constant), Err(_)) => (expr_right, constant),
+            _ => Err("invalid result")?,
+        };
+        println!("{} = {}", left, right);
+
+        while left != "humn" {
+            let current = monkeys.get(left).ok_or("invalid input")?;
+            let (a_ref, b_ref) = match current {
+                Monkey::Add(a, b) | Monkey::Sub(a, b) | Monkey::Mul(a, b) | Monkey::Div(a, b) => {
+                    (a, b)
+                }
+                Monkey::Num(a) => panic!("num"),
+                Monkey::Equ(_, _) => panic!("equ"),
+            };
+            let (a_res, b_res) = (
+                calc_monkey_2(a_ref, &monkeys, &mut HashMap::new()),
+                calc_monkey_2(b_ref, &monkeys, &mut HashMap::new()),
+            );
+            let (a, b) = match (&a_res, &b_res) {
+                (Ok(c), Err(_)) => (c, b_ref),
+                (Err(_), Ok(c)) => (c, a_ref),
+                (Ok(_), Ok(_)) => panic!("ok"),
+                (Err(_), Err(_)) => panic!("err"),
+            };
+
+            match current {
+                Monkey::Add(_, _) => right -= a,
+                Monkey::Sub(a1, b1) if a_res.is_err() => right += a,
+                Monkey::Sub(a1, b1) => right = a - right,
+                Monkey::Mul(_, _) => right /= a,
+                Monkey::Div(_, _) if a_res.is_err() => right *= a,
+                Monkey::Div(_, _) => right = a / right,
+                Monkey::Num(_) => Err("should not be num")?,
+                Monkey::Equ(_, _) => Err("should not be equ")?,
+            }
+            left = b;
+        }
+
+        Ok(right.to_string())
     }
 }
 
+#[derive(Clone, Debug)]
+enum Asd {
+    String(String),
+    Num(i64),
+}
+
+fn calc_monkey_2(
+    name: &str,
+    monkeys: &HashMap<String, Monkey>,
+    cache: &mut HashMap<String, i64>,
+) -> GenericResult<i64> {
+    if name == "humn" {
+        Err("human")?;
+    }
+
+    if let Some(value) = cache.get(name) {
+        return Ok(*value);
+    }
+
+    let monkey = monkeys.get(name).ok_or("monkey not found")?;
+    let result = match monkey {
+        Monkey::Num(num) => *num,
+        Monkey::Add(a, b) => calc_monkey_2(a, monkeys, cache)?
+            .checked_add(calc_monkey_2(b, monkeys, cache)?)
+            .ok_or("add")?,
+        Monkey::Sub(a, b) => calc_monkey_2(a, monkeys, cache)?
+            .checked_sub(calc_monkey_2(b, monkeys, cache)?)
+            .ok_or("sub")?,
+        Monkey::Mul(a, b) => calc_monkey_2(a, monkeys, cache)? * calc_monkey_2(b, monkeys, cache)?,
+        Monkey::Div(a, b) => calc_monkey_2(a, monkeys, cache)? / calc_monkey_2(b, monkeys, cache)?,
+        _ => Err("invalid monkey")?,
+    };
+    cache.insert(name.to_owned(), result);
+
+    Ok(result)
+}
+
+fn print_eq_simplified(
+    name: &str,
+    monkeys: &HashMap<String, Monkey>,
+    cache: &mut HashMap<String, Asd>,
+) -> Asd {
+    if let Some(value) = cache.get(name) {
+        return value.clone();
+    }
+
+    if name == "humn" {
+        return Asd::String("x".to_owned());
+    }
+
+    let monkey = monkeys.get(name).unwrap();
+    let parts = match monkey {
+        Monkey::Num(num) => (Asd::Num(*num), Asd::Num(0)),
+        Monkey::Add(a, b)
+        | Monkey::Sub(a, b)
+        | Monkey::Mul(a, b)
+        | Monkey::Div(a, b)
+        | Monkey::Equ(a, b) => (
+            print_eq_simplified(a, monkeys, cache),
+            print_eq_simplified(b, monkeys, cache),
+        ),
+    };
+
+    let result = match parts {
+        (Asd::Num(a), Asd::Num(b)) => match monkey {
+            Monkey::Num(_) => Some(Asd::Num(a)),
+            Monkey::Add(_, _) => Some(Asd::Num(a + b)),
+            Monkey::Sub(_, _) => Some(Asd::Num(a - b)),
+            Monkey::Mul(_, _) => Some(Asd::Num(a * b)),
+            Monkey::Div(_, _) => Some(Asd::Num(a / b)),
+            Monkey::Equ(_, _) => panic!(),
+        },
+        _ => None,
+    };
+
+    if let None = result {
+        let result = [parts.0, parts.1]
+            .iter()
+            .map(|x| match x {
+                Asd::String(x) => x.to_owned(),
+                Asd::Num(x) => x.to_string(),
+            })
+            .collect_vec();
+        let (a, b) = (result[0].clone(), result[1].clone());
+
+        return Asd::String(match monkey {
+            Monkey::Add(_, _) => format!("({} + {})", a, b),
+            Monkey::Sub(_, _) => format!("({} - {})", a, b),
+            Monkey::Mul(_, _) => format!("{} * {}", a, b),
+            Monkey::Div(_, _) => format!("{} / {}", a, b),
+            Monkey::Equ(_, _) => format!("{} = {}", a, b),
+            _ => panic!("{:?}", monkey),
+        });
+    } else {
+        return result.unwrap();
+    }
+}
+
+fn print_eq(
+    name: &str,
+    monkeys: &HashMap<String, Monkey>,
+    cache: &mut HashMap<String, String>,
+) -> String {
+    if let Some(value) = cache.get(name) {
+        return value.to_owned();
+    }
+
+    if name == "humn" {
+        return "x".to_owned();
+    }
+
+    let monkey = monkeys.get(name).unwrap();
+    let result = match monkey {
+        Monkey::Num(x) => x.to_string(),
+        Monkey::Add(a, b) => format!(
+            "({} + {})",
+            print_eq(a, monkeys, cache),
+            print_eq(b, monkeys, cache)
+        ),
+        Monkey::Sub(a, b) => format!(
+            "({} - {})",
+            print_eq(a, monkeys, cache),
+            print_eq(b, monkeys, cache)
+        ),
+        Monkey::Mul(a, b) => format!(
+            "{} * {}",
+            print_eq(a, monkeys, cache),
+            print_eq(b, monkeys, cache)
+        ),
+        Monkey::Div(a, b) => format!(
+            "{} / {}",
+            print_eq(a, monkeys, cache),
+            print_eq(b, monkeys, cache)
+        ),
+        Monkey::Equ(a, b) => format!(
+            "{} = {}",
+            print_eq(a, monkeys, cache),
+            print_eq(b, monkeys, cache)
+        ),
+    };
+
+    result
+}
+
 fn calc_monkey(
-    name: &String,
+    name: &str,
     monkeys: &HashMap<String, Monkey>,
     cache: &mut HashMap<String, i64>,
 ) -> GenericResult<i64> {
@@ -37,6 +244,7 @@ fn calc_monkey(
         Monkey::Sub(a, b) => calc_monkey(a, monkeys, cache)? - calc_monkey(b, monkeys, cache)?,
         Monkey::Mul(a, b) => calc_monkey(a, monkeys, cache)? * calc_monkey(b, monkeys, cache)?,
         Monkey::Div(a, b) => calc_monkey(a, monkeys, cache)? / calc_monkey(b, monkeys, cache)?,
+        _ => Err("invalid monkey")?,
     };
     cache.insert(name.to_owned(), result);
 
@@ -77,4 +285,5 @@ enum Monkey {
     Sub(String, String),
     Mul(String, String),
     Div(String, String),
+    Equ(String, String),
 }
--- aoc-lib/src/tests/year2022/day21_test.rs
+++ aoc-lib/src/tests/year2022/day21_test.rs
@@ -4,7 +4,8 @@ use crate::tests::util::*;
 
 #[test]
 fn example_input() {
-    let (mut day, ctx) = setup::<Day21>(r#"
+    let (mut day, ctx) = setup::<Day21>(
+        r#"
 root: pppw + sjmn
 dbpl: 5
 cczh: sllz + lgvd
@@ -19,14 +20,15 @@ sllz: 4
 pppw: cczh / lfqf
 lgvd: ljgn * ptdq
 drzm: hmdt - zczc
-hmdt: 32"#);
+hmdt: 32"#,
+    );
     assert_result(day.part1(&ctx), "152", "solve part 1");
-    //assert_result(day.part2(&ctx), "day21_part2", "solve part 2");
+    assert_result(day.part2(&ctx), "301", "solve part 2");
 }
 
 #[test]
 fn puzzle_input() {
     let (mut day, ctx) = setup_from_file::<Day21>();
-    //assert_result(day.part1(&ctx), "day21_part1", "solve part 1");
-    //assert_result(day.part2(&ctx), "day21_part2", "solve part 2");
+    assert_result(day.part1(&ctx), "194501589693264", "solve part 1");
+    assert_result(day.part2(&ctx), "3887609741189", "solve part 2");
 }
